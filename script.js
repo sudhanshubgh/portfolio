81cadcd4667eed551e7a633663bac3fafa245a3b
Sudhanshu Kumar Singh <36701984+sudhanshubgh@users.noreply.github.com>
2021-10-13 17:22:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Tester", "Freelancer", "Data Analyst"],
+        strings: ["Developer", "Tester", "Freelancer", "Data Analyst", "YouTube Creator"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Tester", "Freelancer", "Data Analyst"],
+        strings: ["Developer", "Tester", "Freelancer", "Data Analyst", "YouTube Creator"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
